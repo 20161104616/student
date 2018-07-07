9429c7cd5787fea1403ffb4105a608a4dfad352e
20161104616 <a20161104616@admindeimac-32.local>
2018-07-07 16:43:20 +0800

    Initial Commit

---
+++ student/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  student
+//
+//  Created by 20161104616 on 18/7/7.
+//  Copyright © 2018年 20161104616. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
